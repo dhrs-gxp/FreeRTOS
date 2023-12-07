f880f48500a689bac9707cd94d916356ce5fcbc5
BZXT <1169906553@qq.com>
2023-12-07 20:52:43 +0800

    在FreeRTOS上实现矩阵按键，可以用的

--- USER/main.c
+++ USER/main.c
@@ -11,6 +11,7 @@
 eTaskState state;
 
 static TaskHandle_t AppTaskCreate_Handle;
+static TaskHandle_t LED1_Task_Handle;
 static TaskHandle_t Key_Task_Handle;
 static TaskHandle_t Uart1_Task_Handle;
 
@@ -29,26 +30,22 @@ static void Uart1_Task(void* parameter)
 	}
 }
 
-uint8_t xTimer_Key_State = 0, xTimer_Enter_Flag = 0;
+uint8_t xTimer_Key_State = 0, xTimer_Enter_Flag = 0, Key_Pressed = 0;
 uint8_t button_value = 0, k1_4 = 0, k5_8 = 0;
 
 static void Key_Task(void* parameter)
 {
 	while(1)
 	{
-		if(xTimer_Enter_Flag == 0)
+		if(Key_Pressed == 0 && xTimer_Enter_Flag == 0)
 		{
 			if((xTimer_Key_State == 0 && read_key5_8_Bits() != 0) || (xTimer_Key_State == 1 && read_key1_4_Bits() != 0))
 			{
 				xTimerReset(xTimer_Key, 0); 
 				xTimer_Enter_Flag = 1; 
-				printf("这里是进入软件定时器服务函数之前\r\n");
 			}
-			else if(xTimer_Key_State == 2 && read_key5_8_Bits() == 0)
-			{
-				xTimer_Key_State = 0;
-				printf("%d\r\n", button_value); 
-			}
+			
+			if(xTimer_Key_State == 1 && read_key5_8_Bits() == 0) xTimer_Key_State = 0;
 		}
 		vTaskDelay(2);
 	}
@@ -59,7 +56,6 @@ void vTimer_Key_Callback(TimerHandle_t xTimer)
 	switch(xTimer_Key_State)
 	{
 		case 0:
-			printf("这里是软件定时器服务函数1\r\n");
 			k5_8 = read_key5_8_Bits();
 			if(k5_8 != 0) 
 			{
@@ -71,16 +67,13 @@ void vTimer_Key_Callback(TimerHandle_t xTimer)
 			else xTimer_Key_State = 0;
 			break;
 		case 1:
-			printf("这里是软件定时器服务函数2\r\n");
 			k1_4 = read_key1_4_Bits();
 			if(k1_4 != 0)
 			{ 
-				button_value = 0;
 				button_value |= k5_8; 
 				button_value |= k1_4; 
-				xTimer_Key_State = 2;
+				Key_Pressed = 1;
 			}
-			else xTimer_Key_State = 0;
 			KEY1_4_GPIO_Output_Config();
 			KEY5_8_GPIO_Input_Config();
 			set_key1_4_Bits();
@@ -90,6 +83,27 @@ void vTimer_Key_Callback(TimerHandle_t xTimer)
 	xTimer_Enter_Flag = 0;
 }
 
+
+static void LED1_Task(void* parameter)
+{
+	while(1)
+	{
+		if(Key_Pressed == 1 && read_key5_8_Bits() == 0)
+		{
+			printf("%d\r\n", button_value); 
+			if(button_value == 130)
+			{
+				LED1 = ~LED1;
+				button_value = 0;
+			}
+			else button_value = 0;
+			
+			Key_Pressed = 0;
+		}
+		vTaskDelay(2);
+	}
+}
+
 static void AppTaskCreate(void)
 {
 	BaseType_t xReturn = pdPASS;
@@ -97,6 +111,16 @@ static void AppTaskCreate(void)
 	Semaphore_Uart1 = xSemaphoreCreateBinary();
 	
 	taskENTER_CRITICAL();	//进入临界区
+	
+	xReturn = xTaskCreate((TaskFunction_t)LED1_Task,
+						  (const char*   )"LED1_Task",
+						  (uint16_t      )512,
+						  (void*		 )NULL,
+						  (UBaseType_t   )8,
+						  (TaskHandle_t* )&LED1_Task_Handle);
+										
+	if(xReturn == pdPASS) printf("LED1_Task 任务创建成功!\r\n");
+	else printf("LED1_Task 任务创建失败!\r\n");
 						  
 	xReturn = xTaskCreate((TaskFunction_t)Key_Task,
 						  (const char*   )"Key_Task",
@@ -185,13 +209,14 @@ int main(void)
 
 void USART1_IRQHandler(void)                	//串口1中断服务程序
 {
-	u8 Res;
+	//u8 Res;
 	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
 	{
 		if(Semaphore_Uart1 != NULL)
 		{
-			Res = USART_ReceiveData(USART1);
+			//Res = USART_ReceiveData(USART1);
 			//USART_SendData(USART1, Res);
+			USART_ReceiveData(USART1);
 			xSemaphoreGiveFromISR(Semaphore_Uart1, NULL);
 		}
     } 
